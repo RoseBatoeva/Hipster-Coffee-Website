c16736d5327f68dee2c7e34898157bd06ef55f36
RoseBatoeva <58410784+RoseBatoeva@users.noreply.github.com>
2020-06-09 21:53:24 +0800

    update

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-// const navbarOffsetTop =  get navbar distance from top;
-// if (navbar distance === 0 && navbar has class shadow ) {
-//   remove shadow class
-// }
-// if(navbar distance !== 0 && navbar doesn't have class shadow) {
-//   add classShadow
-// }
 const navbar=$('.navbar');
 function toggleNavbarShadow() {
     const navbarOffsetTop=navbar.offset().top;
     if (navbarOffsetTop!==0 && !navbar.hasClass('navbar-shadow')) {
-        $(".navbar").addClass( "navbar-shadow" );
+        $('.navbar').addClass( 'navbar-shadow' );
     } 
-    if (navbarOffsetTop===0 && navbar.hasClass("navbar-shadow")){
-        $(".navbar").removeClass( "navbar-shadow" );
+    if (navbarOffsetTop===0 && navbar.hasClass('navbar-shadow')){
+        $('.navbar').removeClass( 'navbar-shadow' );
     } 
     }
     toggleNavbarShadow();
-    window.addEventListener('scroll',toggleNavbarShadow)
-
-
-////////////////////////////////
-// const navbar=('.navbar');
-// function toggleNavbarShadow() {
-//     $(".navbar").toggleClass( "navbar-shadow" );
-// }
-// toggleNavbarShadow();
-// window.addEventListener('scroll',toggleNavbarShadow)
-
-////////////////////////////////
-
-//    window.addEventListener('scroll',function () {
-//     $(".navbar").toggleClass( "navbar-shadow" );
-//    });
+    window.addEventListener('scroll',toggleNavbarShadow);
