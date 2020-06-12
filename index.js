00ff2a9aa595a7575020e989a790d394265f5e3c
RoseBatoeva <58410784+RoseBatoeva@users.noreply.github.com>
2020-06-12 21:16:57 +0800

    selected Location

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ function toggleNavbarShadow() {
     }
     toggleNavbarShadow();
     window.addEventListener('scroll',toggleNavbarShadow);
+
+
+const select = $('select');
+function showSelectedLocation() {
+    const selectedLocation = select.value;
+    if (selectedLocation==='san-francisco'){
+        this.removeClass('location-hidden');
+        $('.location').addClass('location-hidden');
+    }
+    if (selectedLocation==='new-york'){
+        this.removeClass('location-hidden');
+        $('.location').addClass('location-hidden');
+    }
+    if (selectedLocation==='los-angeles'){
+        this.removeClass('location-hidden');
+        $('.location').addClass('location-hidden');
+    }
+}
+showSelectedLocation();
+select.addEventListener('change',showSelectedLocation);
+
+
+
